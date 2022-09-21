8136b8b7ecd5ae97b42e33400d04f5cd4e03cab3
sherzodqodirov <sherzodqadirov01@gmail.com>
2022-09-21 12:38:23 +0500

    push

--- Carapp/src/index.js
+++ Carapp/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './redux';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +13,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
