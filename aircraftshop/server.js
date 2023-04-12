7b115dbf41e18a8716fc8b5a191827626784050e
sherzod <sherzodqadirov01@gmail.com>
2023-04-12 18:02:29 +0500

    12.04.2023

--- aircraftshop/server.js
+++ aircraftshop/server.js
@@ -4,8 +4,23 @@ require('dotenv').config
 const app=express()
 const port=process.env.PORT||8000;
 
+// application/json
+app.use(express.json);
+
+//application/x-www-from-urlencoded
+app.use(express.urlencoded({extended:true}))
+
+app.use("/static",express.static(__dirname+"assets"))
 app.get('/',(req,res)=>{
-    res.send("server ok")
+    res.send("my app")
 })
 
-mongoose.connect()
+mongoose.connect("mongodb+srv://admin:admin1@cluster0.ib1moir.mongodb.net/test")
+.then(()=>{
+    app.listen(port,()=>{
+        console.log(` server run ${port}`);
+    })
+})
+.catch(()=>{
+    console.log("server error");
+})
